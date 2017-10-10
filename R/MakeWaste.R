459de07180dd05081eddff8f66efe6f7f618773e
Aaron Robotham <aaron.robotham@uwa.edu.au>
2017-10-10 16:20:24 +0800

    Better waste colours

--- R/MakeWaste.R
+++ R/MakeWaste.R
@@ -32,42 +32,26 @@ function (data = "find", directory = "test", save = FALSE, type = "png",
                 height = 480/72, onefile = TRUE)
         }
     }
-    fibtot = {
-    }
-    fibmax = {
-    }
-    fibmaxpos = {
-    }
+    fibtot = {}
+    fibmax = {}
+    fibmaxpos = {}
     for (i in 1:length(data$fibstats[, 1])) {
-        fibtot = c(fibtot, length(which(data$fibstats[i, ] == 
-            1)))
-        fibmax = c(fibmax, fibtot[i] + length(which(data$fibstats[i, 
-            ] == 0)) + length(which(data$fibstats[i, ] == 99)))
-        fibmaxpos = c(fibmaxpos, fibtot[i] + length(which(data$fibstats[i, 
-            ] == 0)))
+        fibtot = c(fibtot, length(which(data$fibstats[i, ] == 1)))
+        fibmax = c(fibmax, fibtot[i] + length(which(data$fibstats[i,] == 0)) + length(which(data$fibstats[i, ] == 99)))
+        fibmaxpos = c(fibmaxpos, fibtot[i] + length(which(data$fibstats[i,] == 0)))
     }
     par(mar = c(3.1, 3.1, 2.1, 2.1))
     magplot(cbind(data$tilelim[, 1], fibtot), type = "l", xlab = "Tile No.", 
-        ylab = "Fibres", xlim = c(min(data$tilelim[, 1]), max(c(min(data$tilelim[, 
-            1]) + 20, max(data$tilelim[, 1])))), ylim = c(0, 
-            400), col = "purple", main = paste("Fibre Priorities for ",min(data$assign[, 2]),"-", max(data$assign[, 2]), " Tiles", sep = ""), grid=TRUE)
-    legendtext = c("Fibs", "Non-Broken", "Total Fibres")
+        ylab = "Fibres", xlim = c(min(data$tilelim[, 1]), max(c(min(data$tilelim[,1]) + 20, max(data$tilelim[, 1])))), ylim = c(0, 400), col = "black", main = paste("Fibre Priorities for ",min(data$assign[, 2]),"-", max(data$assign[, 2]), " Tiles", sep = ""), grid=TRUE, lwd=2)
     for (i in 1:length(which(PriConvMat[, "WastePlot"] == TRUE))) {
       select=data$assign[,3] == PriConvMat[PriConvMat[, "WastePlot"] == TRUE,1][i] & data$assign[, 2] > 0
       if(length(which(select))>0){
-        lines(as.matrix(as.data.frame(table(data$assign[select, 2]))), lty = PriConvMat[PriConvMat[, 
-            "WastePlot"] == TRUE, "Lty"][i], col = PriConvMat[PriConvMat[, 
-            "WastePlot"] == TRUE, "Colour"][i])
-        legendtext = c(legendtext, paste("Priority", PriConvMat[PriConvMat[, 
-            "WastePlot"] == TRUE, 1][i]))
+        lines(as.matrix(as.data.frame(table(data$assign[select, 2]))), lty = PriConvMat[PriConvMat[, "WastePlot"] == TRUE, "Lty"][i], col = PriConvMat[PriConvMat[, "WastePlot"] == TRUE, "Colour"][i], lwd=2)
       }
     }
-    lines(cbind(data$tilelim[, 1], fibmax), col = "grey", lty = 2)
-    lines(cbind(data$tilelim[, 1], fibmaxpos), col = "red", lty = 3)
-    legend("bottomright", lty = c(2, 3, 1, PriConvMat[PriConvMat[, 
-        "WastePlot"] == TRUE, "Lty"]), legend = legendtext, col = c("grey", 
-        "red", "purple", PriConvMat[PriConvMat[, "WastePlot"] == 
-            TRUE, "Colour"]), bg='white')
+    lines(cbind(data$tilelim[, 1], fibmax), col = "grey", lty = 2, lwd=2)
+    lines(cbind(data$tilelim[, 1], fibmaxpos), col = "red", lty = 3, lwd=2)
+    legend("bottomright", lty = c(2, 3, 1, PriConvMat[PriConvMat[,"WastePlot"], "Lty"]), legend = c("All Fibs", "Non-Broken", "Total Assigned", paste('Priority',PriConvMat[PriConvMat[, "WastePlot"],'Orig'])), col = c("grey", "red", "black", PriConvMat[PriConvMat[, "WastePlot"], "Colour"]), lwd=2, bg='white')
     if (save) {
         dev.off()
     }
